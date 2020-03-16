7b1ff69ce54131c9fbc06a46f4420abaffe7c297
L-Blanc <55198913+L-Blanc@users.noreply.github.com>
2020-03-15 23:49:25 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# scraping-project
+#Niche School Scraper
+
+For this project, I used a combination of the Selenium and BeautifulSoup python libraries to scrape school information (school name, school distrct/city, total student count, student to teacher ratio, and overall Niche grade) from public K-12 schools in Florida.
+
+page url(https://www.niche.com/k12/search/best-public-schools/s/florida/)
+
+- First, I used the variable csvfile to open a csv file named "schools.csv" to collect the information I would be scraping from the page.
+To that csv, I used the writerow function to create a row for each item I would be scraping.
+
+-Next, I created a for loop to click a button and iterate over each of the 153 pages related to my search results.
+
+-Then, inside of that for loop, I collected all of  the divs off of the page that had the class "search result" in the variable "list_of_schools" so that I would ONLY collect the school results related to my filtered search ("public school", "Florida") and not pick up any of the divs containing the sponsored results throughout the page.
+
+- Next, I created a function (scrape_one_school()) to scrape the information for one school and put a for loop in that function iterates over items in the list created by the variable list_of_schools.
+
+- Within the function scrape_one_school,  I created a list (school_details) that returns the information I scraped from each div into a list.
+
+- Those list items are then written in order into each row of the csv file schools.csv
+
+- To ensure the items are being collected, I created another for loop that iterates over the list (school_details) and prints each schools information in the terminal.
+
+The most difficulty I had was chosing a comfortable sleep time that would let me bypass bot captchas.
+
+I also had difficulty figuring out how to embedd my scraping for loop into my button clicking for loop so that the scrape_one_school() function would run on each page.
---
+++ school-1.py
@@ -0,0 +1,98 @@
+import time
+import csv
+import requests
+from selenium import webdriver
+from random import randint
+from bs4 import BeautifulSoup
+
+
+
+driver = webdriver.Chrome(r'C:\Users\flowe\Documents\python\env\scraping\chromedriver')
+
+driver.get("https://www.niche.com/k12/search/best-public-schools/s/florida/")
+
+    #open the csvfile
+csvfile = open('schools.csv', "w", newline="", encoding='utf-8')
+#     #make python writer object
+c = csv.writer(csvfile)
+#     #write each row to csvfile
+c.writerow(["school_name", "district_or_city", "total_count_of_students", "student_teacher_ratio", "niche_grade"])
+# csv_list = []
+
+for n in range(3):
+    page = driver.page_source
+    soup = BeautifulSoup(page, "html.parser")
+    try:
+        driver.find_element_by_css_selector('.icon-arrowright-thin--pagination').click()
+        driver.find_element_by_css_selector('.cookie-banner').click()
+    except:
+        pass
+    s = randint(1, 11)
+        # sleep that number of seconds
+    time.sleep(s)
+
+
+
+    list_of_schools = soup.find_all("div", class_="search-result")
+
+    def scrape_one_school(school):
+        for item in list_of_schools:
+            total_scope=0
+            public_school = item.find("div", class_="search-result")
+            school_names = item.find('h2', class_="search-result__title").get_text()
+            niche_grade = item.find("div",class_="niche__grade--small--a-plus").get_text()
+            district_or_city = item.find("li", class_="search-result-tagline__item").get_text()
+            student_count = item.find_all('span', class_="search-result-fact__value")
+
+            student_teacher_ratio = student_count[0].get_text()
+            total_count = student_count[1].get_text()
+
+            school_details = [school_names, district_or_city, student_teacher_ratio, total_count, niche_grade]
+
+            c.writerow(school_details)
+
+            for i in school_details:
+                try:
+                   print(i) + print()
+                except:
+                    pass
+scrape_one_school(list_of_schools)
+driver.quit()
+#for name in school_names:
+#    print(name.get_text())
+
+#for ratio in student_teacher_ratio:
+    #    print(ratio.get_text())
+
+#for grade in niche_grade:
+    #print(grade.get_text())
+
+#for district in district_or_city:
+    #print(district.get_text())
+
+#def get_single_url(school_url):
+#    page = requests.get(my_url)
+    #soup = BeautifulSoup(page.text, "html.parser")
+    #school_urls = []
+#my_url = "https://www.niche.com/k12/search/best-public-schools/s/florida/"
+
+    #list_of_schools = soup.find('div', class_="search-results")
+    #school_links = list_of_schools.find_all("div", class_="card")
+    #rows = school_links.find_all('li')
+
+
+#get_single_url(my_url)
+    #for row in rows:
+    #    cells = row.find_all('a')
+    #    try:
+        #    links = cells[0].a.attrs['href']
+        #    teacher_urls.append(links)
+        #except:
+        #    pass
+
+    #    return teacher_urls
+
+#url_list = get_single_url(my_url)
+#
+
+#driver.quit()
